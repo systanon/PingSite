1330729b5795c73a0e754c70f6727ff480e967f0
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2024-12-13 18:38:04 +0100

    feat: add interval process call

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,42 @@
-import { getConfig } from "./clireader";
+import { getConfig } from './clireader';
 
 export type Config = {
-    interval: number;
-    maxConcurrencyRequest: number;
-    latencyLimit: number;
-    verbose: boolean;
-
-}; 
+  interval: number;
+  maxConcurrencyRequest: number;
+  latencyLimit: number;
+  verbose: boolean;
+};
 type Res = {
-  url: string,
-  statusCode: number,
-  start: number,
-  latency: number, 
-  slow: boolean,
-}
+  url: string;
+  statusCode: number;
+  start: number;
+  latency: number;
+  slow: boolean;
+};
 
+type Client = (url: string) => Promise<Res>;
 
-type Client = (url: string) => Promise<Res>
-
-const cliConfig = getConfig()
+const cliConfig = getConfig();
 
 if (!cliConfig.success) {
-  throw new Error(cliConfig.error)
+  throw new Error(cliConfig.error);
 }
 
-const { urls, config } = cliConfig
-
+const { urls, config } = cliConfig;
+console.log('TCL: config', config);
 
 const client: Client = async (url: string) => {
-  const start = Date.now()
+  const start = Date.now();
 
-  const statusCode: number = await fetch(url).then(async (res): Promise<number> => {
-    return res.status
-  }).catch(async (res): Promise<number> => {
-    return res.status
-  })
-  
-  const latency = Date.now() - start
+  const statusCode: number = await fetch(url)
+    .then(async (res): Promise<number> => {
+      return res.status;
+    })
+    .catch(async (res): Promise<number> => {
+      return res.status;
+    });
+
+  const latency = Date.now() - start;
 
   return {
     url,
@@ -44,38 +44,69 @@ const client: Client = async (url: string) => {
     start,
     latency,
     slow: false,
-  }
-}
+  };
+};
 
-const process1 = async (queue: Array<string>, client: Client, print: (responses: Array<Res>) => void, config: Config): Promise<Array<Res>> => {
-  const MAX = config.maxConcurrencyRequest
-  const responses: Array<Res> = []
+const process1 = async (
+  urls: Array<string>,
+  client: Client,
+  config: Config,
+): Promise<Array<Res>> => {
+  const _urls = [...urls];
+  const MAX = config.maxConcurrencyRequest;
+  const responses: Array<Res> = [];
   const greenTread = async () => {
-    while (queue.length > 0) {
-      const url = queue.pop()
-      if (!url) break
+    while (_urls.length > 0) {
+      const url = _urls.pop();
+      if (!url) break;
 
-      const res: Res = await client(url)
+      const res: Res = await client(url);
 
-      responses.push(res)
+      responses.push(res);
+    }
+  };
+
+  const promises = new Array(MAX).fill(() => {}).map(greenTread);
+  await Promise.all(promises);
+  return responses;
+};
+
+const printSilence = (responses: Array<Res>) => {};
+const printVerbose = (responses: Array<Res>) => {
+  console.log(responses);
+};
+
+const run = async (
+  urls: Array<string>,
+  client: Client,
+  print: (responses: Array<Res>) => void,
+  config: Config,
+) => {
+  let firstStart = false;
+  let lastExecutionTime = 0;
+
+  const processLoop = async () => {
+    if (!firstStart) {
+      firstStart = true;
+      const responses = await process1(urls, client, config);
+      lastExecutionTime = Date.now();
+      print(responses);
+    }
+    if (Date.now() - lastExecutionTime >= config.interval) {
+      const responses = await process1(urls, client, config);
+      lastExecutionTime = Date.now();
+      print(responses);
+    }
+  };
+
+  while (true) {
+    try {
+      await processLoop();
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    } catch {
+      break;
     }
   }
+};
 
-  const promises = new Array(MAX).fill(() => {}).map(greenTread)
-  await Promise.all(promises)
-  print(responses)
-  return responses
-}
-
-const printSilence = (responses: Array<Res>) => { }
-const printVerbose = (responses: Array<Res>) => { console.log(responses)}
-
-
-
-const run = (urls: Array<string>, client: Client, print: (responses: Array<Res>) => void, config: Config) => {
-  const responses = process1(urls, client,print , config)
-}
-
-run(urls, client, config.verbose ? printVerbose : printSilence, config)
-
-
+run(urls, client, config.verbose ? printVerbose : printSilence, config);
