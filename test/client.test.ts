c69f0875a4fb1c748d2cbddc0774f35ad611e97c
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2024-12-18 23:44:59 +0100

    fix: tests

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,50 +1,43 @@
-import { strict as assert } from "assert";
-import { client } from "../src/client";
-import { delay } from '../src/helpers/delay'
+import { describe, it } from 'node:test';
+import { strict as assert } from 'assert';
+import { client } from '../src/client';
+import { delay } from '../src/helpers/delay';
 
-(global as any).fetch = async (url: string) => {
-  if (url === "https://example.com/success") {
-    await delay(500);
-    return { status: 200 };
-  }
-  if (url === "https://example.com/failure") {
-    await delay(500);
-    throw { status: 500 };
-  }
-  throw new Error("Network Error");
-};
-
-export const clientTest = async () => {
-  try {
-    console.log("Running tests for clientTest...");
-
-    // Test 1: Successful request
-    const result1 = await client("https://example.com/success", 300);
-    assert.strictEqual(result1.url, "https://example.com/success");
+describe('Using client', () => {
+  (global as any).fetch = async (url: string) => {
+    if (url === 'https://example.com/success') {
+      await delay(500);
+      return { status: 200 };
+    }
+    if (url === 'https://example.com/failure') {
+      await delay(500);
+      throw { status: 500 };
+    }
+    throw new Error('Network Error');
+  };
+  it('Successful request', async () => {
+    const result1 = await client('https://example.com/success', 300);
+    assert.strictEqual(result1.url, 'https://example.com/success');
     assert.strictEqual(result1.statusCode, 200);
-    assert.ok(result1.latency >= 0, "Latency should be non-negative");
+    assert.ok(result1.latency >= 0, 'Latency should be non-negative');
     assert.strictEqual(result1.slow, true);
-    console.log("Test 1 passed: Successful request");
-
-    // Test 2: Request fails with status 500
-    const result2 = await client("https://example.com/failure", 600);
-    assert.strictEqual(result2.url, "https://example.com/failure");
+  });
+  it('Failed request', async () => {
+    const result2 = await client('https://example.com/failure', 600);
+    assert.strictEqual(result2.url, 'https://example.com/failure');
     assert.strictEqual(result2.statusCode, 500);
-    assert.ok(result2.latency >= 0, "Latency should be non-negative");
+    assert.ok(result2.latency >= 0, 'Latency should be non-negative');
     assert.strictEqual(result2.slow, false);
-    console.log("Test 2 passed: Request fails with status 500");
-
-    // Test 3: Network error
-    const result3 = await client("https://example.com/error", 500);
-    assert.strictEqual(result3.url, "https://example.com/error");
-    assert.strictEqual(result3.statusCode, undefined, "Should handle network errors without a status code");
-    assert.ok(result3.latency >= 0, "Latency should be non-negative");
+  });
+  it('Network error', async () => {
+    const result3 = await client('https://example.com/error', 500);
+    assert.strictEqual(result3.url, 'https://example.com/error');
+    assert.strictEqual(
+      result3.statusCode,
+      undefined,
+      'Should handle network errors without a status code',
+    );
+    assert.ok(result3.latency >= 0, 'Latency should be non-negative');
     assert.strictEqual(result3.slow, false);
-    console.log("Test 3 passed: Network error");
-
-    console.log("All tests for clientTest passed!");
-  } catch (error) {
-    console.error("A test for clientTest failed:", error.message);
-    process.exit(1);
-  }
-};
+  });
+});
--- test/print.test.ts
+++ test/print.test.ts
@@ -1,66 +1,125 @@
-import { strict as assert } from "assert";
-import { Res } from "../src/types/global"
-import { printSilence, validate } from "../src/prints"; 
+import { describe, it } from 'node:test';
+import { strict as assert } from 'assert';
+import { Res } from '../src/types/global';
+import { printSilence, validate } from '../src/prints';
 
-export const printTests = () => {
-const mockConsoleLog = () => {
-  const logs: any[] = [];
-  const originalConsoleLog = console.log;
+describe('Print result', () => {
+  describe('Validate response', () => {
+    it('Failed requests', () => {
+      assert.strictEqual(
+        validate({
+          statusCode: 400,
+          url: 'https://example.com',
+          start: Date.now(),
+          latency: 200,
+          slow: false,
+        }),
+        true,
+      );
+      assert.strictEqual(
+        validate({
+          statusCode: 500,
+          url: 'https://example.com',
+          start: Date.now(),
+          latency: 200,
+          slow: false,
+        }),
+        true,
+      );
+      assert.strictEqual(
+        validate({
+          statusCode: 304,
+          url: 'https://example.com',
+          start: Date.now(),
+          latency: 200,
+          slow: false,
+        }),
+        true,
+      );
+      assert.strictEqual(
+        validate({
+          statusCode: 200,
+          url: 'https://example.com',
+          start: Date.now(),
+          latency: 200,
+          slow: true,
+        }),
+        true,
+      );
+    });
+    it('successful requests', () => {
+      assert.strictEqual(
+        validate({
+          statusCode: 200,
+          url: 'https://example.com',
+          start: Date.now(),
+          latency: 200,
+          slow: false,
+        }),
+        false,
+      );
+      assert.strictEqual(
+        validate({
+          statusCode: 200,
+          url: 'https://example.com',
+          start: Date.now(),
+          latency: 201,
+          slow: false,
+        }),
+        false,
+      );
+    });
 
-  console.log = (...args: any[]) => {
-    logs.push(...args);
-  };
+    it('with delayed response', () => {
+      assert.strictEqual(
+        validate({
+          statusCode: 200,
+          url: 'https://example.com',
+          start: Date.now(),
+          latency: 200,
+          slow: true,
+        }),
+        true,
+      );
+    });
+  });
+  describe('Print silence', () => {
+    const mockConsoleLog = () => {
+      const logs: any[] = [];
+      const originalConsoleLog = console.log;
 
-  return {
-    logs,
-    restore: () => {
-      console.log = originalConsoleLog;
-    },
-  };
-};
+      console.log = (...args: any[]) => {
+        logs.push(...args);
+      };
 
-try {
-  console.log("Testing validate...");
+      return {
+        logs,
+        restore: () => {
+          console.log = originalConsoleLog;
+        },
+      };
+    };
+    const mockResponses: Res[] = [
+      { statusCode: 400, url: 'https://example.com', start: Date.now(), latency: 200, slow: false },
+      { statusCode: 400, url: 'https://example.com', start: Date.now(), latency: 200, slow: false },
+      { statusCode: 500, url: 'https://example.com', start: Date.now(), latency: 200, slow: false },
+      { statusCode: 200, url: 'https://example.com', start: Date.now(), latency: 200, slow: true },
+      { statusCode: 200, url: 'https://example.com', start: Date.now(), latency: 200, slow: false },
+    ];
 
-  assert.strictEqual(validate({ statusCode: 400, url: "https://example.com", start: Date.now(), latency: 200, slow: false }), true);
-  assert.strictEqual(validate({ statusCode: 500, url: "https://example.com", start: Date.now(), latency: 200, slow: false  }), true);
-  assert.strictEqual(validate({ statusCode: 200, url: "https://example.com", start: Date.now(), latency: 200, slow: false }), false);
-  assert.strictEqual(validate({ statusCode: 304, url: "https://example.com", start: Date.now(), latency: 200, slow: false  }), true);
-  assert.strictEqual(validate({ statusCode: 200, url: "https://example.com", start: Date.now(), latency: 200, slow: true  }), true);
+    const expectedOutput = [
+      { statusCode: 400, url: 'https://example.com', start: Date.now(), latency: 200, slow: false },
+      { statusCode: 400, url: 'https://example.com', start: Date.now(), latency: 200, slow: false },
+      { statusCode: 500, url: 'https://example.com', start: Date.now(), latency: 200, slow: false },
+      { statusCode: 200, url: 'https://example.com', start: Date.now(), latency: 200, slow: true },
+    ];
 
-  console.log("Test for validade Done!");
+    it('Print only failed response and with lond answers', () => {
+      const mock = mockConsoleLog();
+      printSilence(mockResponses);
+      mock.restore();
 
-  console.log("Testing printSilence...");
-
-  const mockResponses: Res[] = [
-    { statusCode: 400, url: "https://example.com", start: Date.now(), latency: 200, slow: false },
-    { statusCode: 400, url: "https://example.com", start: Date.now(), latency: 200, slow: false },
-    { statusCode: 500, url: "https://example.com", start: Date.now(), latency: 200, slow: false  },
-    { statusCode: 200, url: "https://example.com", start: Date.now(), latency: 200, slow: true  },
-    { statusCode: 200, url: "https://example.com", start: Date.now(), latency: 200, slow: false  },
-  ];
-
-  const expectedOutput = [
-    { statusCode: 400, url: "https://example.com", start: Date.now(), latency: 200, slow: false },
-    { statusCode: 400, url: "https://example.com", start: Date.now(), latency: 200, slow: false },
-    { statusCode: 500, url: "https://example.com", start: Date.now(), latency: 200, slow: false  },
-    { statusCode: 200, url: "https://example.com", start: Date.now(), latency: 200, slow: true  },
-
-  ];
-
-  const mock = mockConsoleLog();
-  printSilence(mockResponses);
-  mock.restore();
-
-  assert.deepStrictEqual(mock.logs, [expectedOutput]);
-
-  console.log("Test for printTests done!");
-
-  console.log("All tests for printTests passed successfully!");
-} catch (error) {
-  console.error("Tests for printTests failed:");
-  console.error(error.message);
-  process.exit(1);
-}
-
-}
+      assert.deepStrictEqual(mock.logs, [expectedOutput]);
+    });
+  });
+});
